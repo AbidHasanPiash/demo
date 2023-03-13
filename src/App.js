d34beee6b0c5b88d9a1961424a93d8891a0a921c
Abid <meghpiash@gmail.com>
2023-03-13 15:32:59 +0600

    init react router and create login signup page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-4xl text-violet-500">Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          <Route index element={<Home/>} />
+          <Route exact  path="/login" element={<Login />} />
+          <Route exact  path="/signup" element={<Signup />} />
+          <Route path="*" element={<div className="flex items-center justify-center">404 error</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 

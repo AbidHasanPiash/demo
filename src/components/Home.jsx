b35affb226804579b8e32a67a637674ec33b0871
Abid <meghpiash@gmail.com>
2023-03-13 15:41:25 +0600

    home page basic design

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>
-        Home
+    <div className='flex flex-col items-center justify-center h-screen space-y-10'>
+        <h1>Home page</h1>
+        <div className='space-x-6'>
+            <button className='rounded-md py-2 px-6 bg-blue-500 hover:bg-blue-600 text-white'>Log Out</button>
+        </div>
+        
     </div>
   )
 }

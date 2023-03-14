934e69435a742ea150208b38ed4f4d4adab957dc
Abid <meghpiash@gmail.com>
2023-03-14 18:37:41 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Dashboard";
+import Dashboard from "./pages/Dashboard";
 import Login from "./auth/Login";
 import Signup from "./auth/Signup";
 import ForgotPassword from "./auth/ForgotPassword";
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route>
-          <Route index element={<Home/>} />
+          <Route index element={<Dashboard/>} />
           <Route exact  path="/login" element={<Login />} />
           <Route exact  path="/signup" element={<Signup />} />
           <Route exact  path="/forgotPassword" element={<ForgotPassword />} />

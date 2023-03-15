b4c44e8088cdfb29ec8e2c1fd21eada8acf9f94e
Abid <meghpiash@gmail.com>
2023-03-15 12:05:03 +0600

    show active tab when click a sidebar tab

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,32 @@
 import React from 'react'
 
-export default function Sidebar() {
+export default function Sidebar({tab}) {
+  const handleMenuItemClick = (t) => {
+    tab(t);
+  }
+  const menuItems = [
+    {id:1, name:'Dashboard', icon:''},
+    {id:2, name:'Report', icon:''},
+    {id:3, name:'Products', icon:''},
+    {id:4, name:'Order', icon:''},
+    {id:5, name:'Purchase', icon:''},
+    {id:6, name:'Setup', icon:''},
+    {id:7, name:'Company', icon:''},
+    {id:8, name:'Suplier', icon:''},
+    {id:9, name:'Employe', icon:''}
+  ]
   return (
     <aside className="bg-gray-900 text-gray-500 w-64 h-screen px-4 py-6">
     <nav>
       <ul>
-        <li className="mb-4">
-          Dashboard
-        </li>
-        <li className="mb-4">
-          Report
-        </li>
-        <li className="mb-4">
-          User
-        </li>
+        {menuItems.map(item=>(
+          <li 
+          key={item.id} 
+          onClick={()=>{handleMenuItemClick(item.name)}} 
+          className="mb-4">
+            {item.name}
+          </li>
+        ))}
       </ul>
     </nav>
   </aside>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,49 +8,48 @@ import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../Firebase";
 
 export default function Dashboard() {
+
   const navigate = useNavigate();
   const [authUser, setAuthUser] = useState(null);
+
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/firebase.User
-        // const uid = user.uid;
-        // ...
         setAuthUser(user);
       } else {
-        // User is signed out
-        // ...
         setAuthUser(null);
         navigate("/login");
       }
     });
   }, [navigate]);
+
   const Logout = () => {
     signOut(auth)
       .then(() => {
-        // Sign-out successful.
         navigate("/login");
       })
       .catch((error) => {
-        // An error happened.
         console.log(error);
       });
   };
 
+  const [activeTab, setAtciveTab] = useState(null);
+  const handleActiveTab = (tab) => {
+    setAtciveTab(tab);
+  }
+
   return (
     <div className="flex w-screen">
       <div className="">
-        <Sidebar />
+        <Sidebar tab = {handleActiveTab}/>
       </div>
       <div className="w-screen">
         <div className="flex flex-col">
           <Header />
           <div>
             <h1>Home page</h1>
-            <h1>
-              {authUser ? <p>Welcome {authUser.email}</p> : <p>Please Login</p>}
-            </h1>
+            <h1> {authUser ? <p>Welcome {authUser.email}</p> : <p>Please Login</p>} </h1>
+            <h1>{activeTab}</h1>
             <button
               onClick={Logout}
               className="rounded-md py-2 px-6 bg-blue-500 hover:bg-blue-600 text-white"

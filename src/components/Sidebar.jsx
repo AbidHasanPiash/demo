92b881828f941cd293b19155d1476169129ad70b
Abid <meghpiash@gmail.com>
2023-03-16 14:10:24 +0600

    sidebar half ok

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
+import logo from '../media/logo.png'
+import profile from '../media/profile.png'
 import {HiOutlineBell} from 'react-icons/hi'
+import {RiSearch2Line} from 'react-icons/ri'
 import SidebarItem from './SidebarItem';
 
 export default function Sidebar({tab}) {
@@ -65,6 +68,31 @@ export default function Sidebar({tab}) {
   ]
   return (
     <aside className="bg-gray-700 text-gray-300 w-64 h-screen px-2 py-6">
+      <div>
+        <div className='flex items-center justify-start border-b border-gray-500 space-x-2 p-3 pl-4'>
+          <img 
+            className='w-8 rounded-full ring-2 ring-white'
+            src={logo} 
+            alt="profile" 
+          />
+          <h1 className='text-xl font-semibold'>CreativeHI <span className='text-sm text-gray-400'>demo</span></h1>
+        </div>
+        <div className='flex items-center justify-start border-b border-gray-500 space-x-2 p-4 pl-4'>
+          <img 
+            className='w-8 rounded-full'
+            src={profile} 
+            alt="profile" 
+          />
+          <h1 className='text-lg'>Elon Musk</h1>
+        </div>
+        <div className='flex items-center justify-start border border-gray-500 my-4 rounded'>
+          <input 
+            type="text" 
+            placeholder='Search' 
+            className='w-full h-8 bg-gray-600 rounded-l outline-none pl-2 border-r border-gray-500 focus:ring-1'/>
+          <RiSearch2Line className='w-14 text-white '/>
+        </div>
+      </div>
     <nav>
       <ul className='flex flex-col'>
         {menuItems.map((item, index) => 

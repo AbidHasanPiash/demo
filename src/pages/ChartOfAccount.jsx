4510be90bab5821c34769ec767e43962b868d73c
Abid <meghpiash@gmail.com>
2023-03-21 16:39:53 +0600

    setup table lable for coa table

--- src/pages/ChartOfAccount.jsx
+++ src/pages/ChartOfAccount.jsx
@@ -3,44 +3,45 @@ import CoaTableItem from "./coaContent/CoaTableItem";
 
 export default function ChartOfAccount() {
   const data = [
-    { id: 1, name: "C", size: "", date: "02/19/2010", children: [
-        { id: 11, name: "Program Files", size: "120MB", date: "03/20/2010", 
+    { id: 1, level: 1, name: "C", size: "", date: "02/19/2010", children: [
+        { id: 11, level: 2, name: "Program Files", size: "120MB", date: "03/20/2010", 
           children: [
-            { id: 111, name: "Java", size: "", date: "01/13/2010", state: "closed", 
+            { id: 111, level: 3, name: "Java", size: "", date: "01/13/2010", state: "closed", 
               children: [
-                { id: 1111, name: "java.exe", size: "142KB", date: "01/13/2010", },
-                { id: 1112, name: "jawt.dll", size: "5KB", date: "01/13/2010", },
+                { id: 1111, level: 4, name: "java.exe", size: "142KB", date: "01/13/2010", },
+                { id: 1112, level: 4, name: "jawt.dll", size: "5KB", date: "01/13/2010", },
               ],
             },
-            { id: 122, name: "MySQL", size: "", date: "01/13/2010", state: "closed",
+            { id: 112, level: 3, name: "MySQL", size: "", date: "01/13/2010", state: "closed",
               children: [
-                { id: 221, name: "my.ini", size: "10KB", date: "02/26/2009", },
-                { id: 222, name: "my-huge.ini", size: "5KB", date: "02/26/2009", },
-                { id: 223, name: "my-large.ini", size: "5KB", date: "02/26/2009", },
+                { id: 1121, level: 4, name: "my.ini", size: "10KB", date: "02/26/2009", },
+                { id: 1122, level: 4, name: "my-huge.ini", size: "5KB", date: "02/26/2009", },
+                { id: 1123, level: 4, name: "my-large.ini", size: "5KB", date: "02/26/2009", },
               ],
             },
           ],
         },
-        { id: 12, name: "eclipse", size: "", date: "01/20/2010",
+        { id: 12, level: 2, name: "eclipse", size: "", date: "01/20/2010",
           children: [
-            { id: 121, name: "eclipse.exe", size: "56KB", date: "05/19/2009", },
-            { id: 122, name: "eclipse.ini", size: "1KB", date: "04/20/2010", },
-            { id: 123, name: "notice.html", size: "7KB", date: "03/17/2005", },
+            { id: 121, level: 3, name: "eclipse.exe", size: "56KB", date: "05/19/2009", },
+            { id: 122, level: 3, name: "eclipse.ini", size: "1KB", date: "04/20/2010", },
+            { id: 123, level: 3, name: "notice.html", size: "7KB", date: "03/17/2005", },
           ],
         },
       ],
     },
   ];
   return (
-  <div className='text-4xl'>
+  <div className='text-lg'>
     <h1>data</h1>
-    <div className="w-3/4">
+    <div className="w-3/4 mt-20">
       <table className='min-w-full table-fixed border-collapse divide-y divide-gray-200'>
         <thead className="bg-gray-50">
           <tr>
-            <th className="w-1/3 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" >Yum</th>
-            <th className="w-1/3 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" >Size</th>
-            <th className="w-1/3 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" >Date</th>
+            <th className="w-1/4 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" >Level</th>
+            <th className="w-1/4 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" >Yum</th>
+            <th className="w-1/4 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" >Size</th>
+            <th className="w-1/4 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" >Date</th>
           </tr>
         </thead>
         <tbody className="bg-white divide-y divide-gray-200">
--- src/pages/coaContent/CoaTableItem.jsx
+++ src/pages/coaContent/CoaTableItem.jsx
@@ -1,29 +1,35 @@
 import React, {useState} from 'react'
+import {BiRightArrow} from 'react-icons/bi';
 
 export default function CoaTableItem({item}) {
   const [expand, setExpand] = useState(false);
+  const itemNameStyle =`px-6 whitespace-nowrap flex
+                        ${item.level === 1  && 'ml-3'}
+                        ${item.level === 2  && 'ml-6'}
+                        ${item.level === 3  && 'ml-9'}
+                        ${item.level === 4  && 'ml-12'}`
   if (item.children){
     return(
         <React.Fragment>
-            <tr 
-            className='space-y-4'
-            onClick={()=>setExpand(!expand)}>
-                <td className="px-6 py-4 whitespace-nowrap">{item.name}</td>
-                <td className="px-6 py-4 whitespace-nowrap">{item.size}</td>
-                <td className="px-6 py-4 whitespace-nowrap">{item.date}</td>
+            <tr onClick={()=>setExpand(!expand)}>
+                <td className="px-6 whitespace-nowrap">{item.level}</td>
+                <td className={itemNameStyle}><BiRightArrow/><p>{item.name}</p></td>
+                <td className="px-6 whitespace-nowrap">{item.size}</td>
+                <td className="px-6 whitespace-nowrap">{item.date}</td>
             </tr>
             {expand && item.children.map((children, index)=>(
-                <CoaTableItem key={index} item = {children} className="pl-10"/>
+              <CoaTableItem key={index} item = {children} className="bg-gray-500"/>
             ))}
         </React.Fragment>
       )
   }
   else{
     return(
-        <tr className='space-y-4'>
-          <td className="pl-20">{item.name}</td>
-          <td className="px-6">{item.size}</td>
-          <td className="px-6">{item.date}</td>
+        <tr>
+          <td className="px-6 whitespace-nowrap">{item.level}</td>
+          <td className={itemNameStyle}>{item.name}</td>
+          <td className="px-6 whitespace-nowrap">{item.size}</td>
+          <td className="px-6 whitespace-nowrap">{item.date}</td>
         </tr>
       )
   }

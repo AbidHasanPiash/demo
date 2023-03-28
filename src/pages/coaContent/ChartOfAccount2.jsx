fc0d16a7204c63801ea1186080bb564be0b8ba27
Abid <meghpiash@gmail.com>
2023-03-28 14:33:20 +0600

    duplicate for experiment

--- src/pages/coaContent/ChartOfAccount2.jsx
+++ src/pages/coaContent/ChartOfAccount2.jsx
@@ -1,12 +1,37 @@
 import React, { useState } from "react";
-import useFetch from "../../hooks/useFetch";
-import CoaTableItem from "./CoaTableItem";
+import CoaTableItem2 from "./CoaTableItem2";
 
-export default function ChartOfAccount() {
-  const {isLoading, data, error } = useFetch(`http://localhost:8000/coaData`);
-  isLoading && <div>Loading...</div>;
-  error && <div>Error: {error.message}</div>;
-  (!data || !data?.length) && <div>No data found</div>;
+export default function ChartOfAccount2() {
+  const data = [
+    { id: 1, name: "C", size: "345", date: "02/19/2010",
+      children: [
+        { id: 2, name: "Program Files", size: "120 MB", date: "03/20/2010",
+          children: [
+            { id: 21, name: "Java", size: "", date: "01/13/2010", state: "closed",
+              children: [
+                { id: 211, name: "java.exe", size: "142 KB", date: "01/13/2010" },
+                { id: 212, name: "jawt.dll", size: "5 KB", date: "01/13/2010" }
+              ]
+            },
+            { id: 22, name: "MySQL", size: "", date: "01/13/2010", state: "closed",
+              children: [
+                { id: 221, name: "my.ini", size: "10 KB", date: "02/26/2009" },
+                { id: 222, name: "my-huge.ini", size: "5 KB", date: "02/26/2009" },
+                { id: 223, name: "my-large.ini", size: "5 KB", date: "02/26/2009" }
+              ]
+            }
+          ]
+        },
+        { id: 3, name: "eclipse", size: "", date: "01/20/2010",
+          children: [
+            { id: 31, name: "eclipse.exe", size: "56 KB", date: "05/19/2009" },
+            { id: 32, name: "eclipse.ini", size: "1 KB", date: "04/20/2010" },
+            { id: 33, name: "notice.html", size: "7 KB", date: "03/17/2005" }
+          ]
+        }
+      ]
+    }
+  ];
   const [editable, setEditable] = useState(false);
   const [selectedRow, setSelectedRow] = useState(null);
   const handleSelectedRow = (row) => {
@@ -34,7 +59,7 @@ export default function ChartOfAccount() {
         </thead>
         <tbody className="bg-white divide-y divide-gray-200">
           {data?.map((item, index) => (
-            <CoaTableItem 
+            <CoaTableItem2 
               key={index} 
               item={item} 
               editable={editable}
---
+++ src/pages/coaContent/CoaTableItem2.jsx
@@ -0,0 +1,64 @@
+import React, {useState} from 'react'
+import {AiFillFolderAdd, AiFillFolderOpen, AiOutlineFileText} from 'react-icons/ai';
+import {BiRightArrow} from 'react-icons/bi';
+import {VscTriangleDown} from 'react-icons/vsc';
+import {RxDot} from 'react-icons/rx';
+
+export default function CoaTableItem2({item, editable, selectedRow, handleSelectedRow}) {
+  const [expand, setExpand] = useState(false);
+  const itemNameStyle =`px-6 whitespace-nowrap flex cursor-pointer
+                        ${item.level === 1  && 'ml-3'}
+                        ${item.level === 2  && 'ml-6'}
+                        ${item.level === 3  && 'ml-9'}
+                        ${item.level === 4  && 'ml-12'}`
+  if (item.children){
+    return(
+        <React.Fragment>
+            <tr className={`${selectedRow === item.id ? 'bg-yellow-300':'bg-white'}`}>
+                <td className={itemNameStyle}>
+                  <div onClick={()=>setExpand(!expand)} className='flex items-center justify-center space-x-2'>
+                    {expand 
+                      ? <span className='flex items-center justify-center'><VscTriangleDown size={15}/> <AiFillFolderOpen size={25}/> </span> 
+                      : <span className='flex items-center justify-center'><BiRightArrow size={15}/> <AiFillFolderAdd size={25}/> </span> 
+                    }
+                  </div>
+                </td>
+                <td className="px-6 whitespace-nowrap"><p onClick={()=>!editable && handleSelectedRow(item.id)}>
+                  {editable && selectedRow === item.id
+                    ?<input type="text" name="name" id="name" value={item.name} />
+                    :<span>{item.name}</span>
+                  }
+                </p></td>
+                <td className="px-6 whitespace-nowrap">{item.size}</td>
+                <td className="px-6 whitespace-nowrap">{item.date}</td>
+                <td className="px-6 whitespace-nowrap">{item.level}</td>
+            </tr>
+            {expand && item.children.map((children, index)=>(
+              <CoaTableItem2 
+                key={index} 
+                item = {children} 
+                editable={editable}
+                selectedRow={selectedRow} 
+                handleSelectedRow={handleSelectedRow}
+              />
+            ))}
+        </React.Fragment>
+      )
+  }
+  else{
+    return(
+        <tr className={`${selectedRow && selectedRow === item.id ? 'bg-yellow-300':'bg-white'}`}>
+          <td className={itemNameStyle}><RxDot/></td>
+          <td className="px-6 whitespace-nowrap">
+            <div className='flex items-center justify-start space-x-2'>
+              <AiOutlineFileText/>
+              <p onClick={()=>handleSelectedRow(item.id)}>{item.name}</p>
+            </div>
+          </td>
+          <td className="px-6 whitespace-nowrap">{item.size}</td>
+          <td className="px-6 whitespace-nowrap">{item.date}</td>
+          <td className="px-6 whitespace-nowrap">{item.level}</td>
+        </tr>
+      )
+  }
+}

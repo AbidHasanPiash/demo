e4b781d72c30f553ea39a7c5554d26c201820c55
Abid <meghpiash@gmail.com>
2023-03-30 11:55:54 +0600

    using react-table and coaTable is expandable now

---
+++ src/pages/coaContent/CoaTable.jsx
@@ -0,0 +1,55 @@
+import { useTable, useExpanded } from "react-table";
+
+export default function CoaTable({ columns, data }) {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state: { expanded },
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useExpanded // Use the useExpanded plugin hook
+  );
+
+  return (
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td className={`${row.isExpanded && 'bg-slate-500'}`} {...cell.getCellProps()}>
+                        {cell.render("Cell")}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <br />
+      <div>Showing the first 20 results of {rows.length} rows</div>
+      <pre>
+        <code>{JSON.stringify({ expanded: expanded }, null, 2)}</code>
+      </pre>
+    </>
+  );
+}

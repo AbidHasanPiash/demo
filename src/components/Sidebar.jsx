683b0db140f1650d70c5dd77c339bf7fcbee87c7
Abid <meghpiash@gmail.com>
2023-04-03 12:10:57 +0600

    new mesage added for sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -54,10 +54,10 @@ export default function Sidebar({tab, isSidebarOpen}) {
     { id:2, name:'Report',  link:'/report', icon:<TbReportSearch size={22}/> },
     { id:3, name:'Chart of Account',  link:'/chartofaccount', icon:<CgListTree size={22}/> },
     { id:3, name:'Chart of Account View',  link:'/chartofaccountview', icon:<CgListTree size={22}/> },
-    { id:4, name:'Products', icon:<TbBrandProducthunt size={22}/>, 
+    { id:4, name:'Products', message:'new', icon:<TbBrandProducthunt size={22}/>, 
       subItems:[
         {id:1, name:'Pharmacy', link:'/pharmacy',  icon:<GiMedicines size={22}/>},
-        {id:2, name:'Non-Pharmacy', link:'/non-pharmacy', icon:<TbBabyBottle size={22}/>}
+        {id:2, name:'Non-Pharmacy', message:'new', link:'/non-pharmacy', icon:<TbBabyBottle size={22}/>}
       ]},
     { id:5, name:'Order', link:'/order', icon:<MdOutlinePlaylistAddCheckCircle size={22}/> },
     { id:6, name:'Purchase', link:'/purchase', icon:<HiOutlineShoppingBag size={22}/> },
--- src/components/SidebarItem.jsx
+++ src/components/SidebarItem.jsx
@@ -13,22 +13,26 @@ export default function SidebarItem({item, tab, aTab, isSidebarOpen}){
             <li>
                 <div onClick={() => setExpand(!expand)}>
                     <span className={`hover:bg-gray-700 flex items-center justify-between cursor-pointer rounded my-1 p-3 select-none`}>
-                        
-                            <div className="flex items-center justify-center space-x-2">
-                                <i className=''>{item.icon}</i>
-                                <p className={`${isSidebarOpen ? 'block' : 'scale-0'} duration-300`}>{item.name}</p>
-                            </div>
-                        <RiArrowLeftSLine size={22}  className={`${isSidebarOpen ? 'block' : 'scale-0'} duration-300 ${expand && '-rotate-90'}`}/>  
+                        <div className="flex items-center justify-center space-x-2">
+                            <i className=''>{item.icon}</i>
+                            <p className={`${isSidebarOpen ? 'block' : 'scale-0'} duration-300`}>{item.name}</p>
+                        </div>
+                        <div className="flex items-center justify-center space-x-2">
+                            {item.message && <span className='bg-rose-600 rounded-md px-2 text-white text-right text-sm font-bold'>{item.message}</span>}
+                            <RiArrowLeftSLine size={22}  className={`${isSidebarOpen ? 'block' : 'scale-0'} duration-300 ${expand && '-rotate-90'}`}/> 
+                        </div> 
                     </span> 
                 </div>
                 <div className={`${isSidebarOpen && expand ? "block" : "hidden"}  pl-3`}>
                     { item.subItems.map((child, index) =>
                         <div key={index} onClick={()=>handleTab(child.name)}>
                             <NavLink to={child.link ? child.link : '#'}>
-                                <span
-                                    className={`${location.pathname === child.link ? 'bg-blue-500 text-white' : 'hover:bg-gray-700'}  flex items-center justify-start cursor-pointer rounded my-1 p-3 space-x-2 select-none`}>
-                                    <i className=''>{child.icon}</i>
-                                    <p className={`${isSidebarOpen ? 'block' : 'scale-0'} duration-300`}>{child.name}</p>  
+                                <span className={`${location.pathname === child.link ? 'bg-blue-500 text-white' : 'hover:bg-gray-700'}  flex items-center justify-between cursor-pointer rounded my-1 p-3 select-none`}>
+                                    <div className="flex space-x-2">
+                                        <i className=''>{child.icon}</i>
+                                        <p className={`${isSidebarOpen ? 'block' : 'scale-0'} duration-300`}>{child.name}</p> 
+                                    </div> 
+                                    {child.message && <span className='bg-rose-600 rounded-md px-2 text-white text-right text-sm font-bold'>{child.message}</span>}
                                 </span>
                             </NavLink>
                         </div>
@@ -40,10 +44,12 @@ export default function SidebarItem({item, tab, aTab, isSidebarOpen}){
         return (
             <li onClick={()=>handleTab(item.name)}>
                 <NavLink to={item.link ? item.link : '#'}>
-                    <span
-                        className={`${location.pathname === item.link ? 'bg-blue-500 text-white' : 'hover:bg-gray-700'} flex items-center justify-start cursor-pointer rounded my-1 p-3 space-x-2 select-none`}>
-                        <i className=''>{item.icon}</i>
-                        <p className={`${isSidebarOpen ? 'block' : 'scale-0'} duration-300`}>{item.name}</p>
+                    <span className={`${location.pathname === item.link ? 'bg-blue-500 text-white' : 'hover:bg-gray-700'} flex items-center justify-between cursor-pointer rounded my-1 p-3 select-none`}>
+                        <div className="flex space-x-2">
+                            <i className=''>{item.icon}</i>
+                            <p className={`${isSidebarOpen ? 'block' : 'scale-0'} duration-300`}>{item.name}</p>
+                        </div>
+                        {item.message && <span className='bg-rose-600 rounded-md px-2 text-white text-right text-sm font-bold'>{item.message}</span>}
                     </span>
                 </NavLink>
                 
